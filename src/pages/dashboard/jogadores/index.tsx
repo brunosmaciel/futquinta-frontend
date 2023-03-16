8f0699c5aa98b58c167bc0376a9d69e955507eb0
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-03-16 09:13:25 -0300

    fix:list players

--- src/pages/dashboard/jogadores/index.tsx
+++ src/pages/dashboard/jogadores/index.tsx
@@ -1,22 +1,54 @@
-import useSWR from 'swr';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-import { PlayerProfile } from '../../../..';
+import { LoadingSpin } from '../../../components/Loading';
+import { getProfileImage } from '../../../functions/functions';
 import { usePlayers } from '../../../hooks/usePlayers';
-
 const Jogadores = () => {
   const { players, isLoading } = usePlayers();
-
+  const { push } = useRouter();
   if (isLoading) {
-    return <h1>carregando</h1>;
+    return <LoadingSpin />;
   }
   if (!isLoading) {
     return (
-      <>
-        <h1>Jogadores PAGE</h1>
-        {players?.map((player) => (
-          <p key={player.id}>{player.name}</p>
-        ))}
-      </>
+      <div className="w-[95%] mx-auto">
+        <div className="flex flex-col ">
+          {players?.map((player) => (
+            <div
+              key={player.id}
+              onClick={() => push(`/dashboard/jogadores/${player.slug}`)}
+              className="flex items-center hover:bg-neutral  transition-all justify-between gap-5 py-2 border-b-[1px] border-neutral-900 w-[90%] mx-auto cursor-pointer"
+            >
+              <div className="flex items-center gap-4">
+                <Link
+                  role={'Foto de perfil do jogador'}
+                  href={`/dashboard/jogadores/${player.slug}`}
+                  className="cursor-pointer"
+                >
+                  <Image
+                    src={getProfileImage(player)}
+                    alt="Foto de perfil do jogador"
+                    width={60}
+                    height={60}
+                    className="rounded-full w-12 h-12 "
+                  />
+                </Link>
+                <h1 className="min-w-20">
+                  {player.shirtNumber || 0} - {player.name}
+                </h1>
+              </div>
+              <Link
+                href={`/dashboard/jogadores/${player.slug}`}
+                className="link text-[12px] self-end justify-end"
+              >
+                Ver perfil completo
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     );
   }
 };
