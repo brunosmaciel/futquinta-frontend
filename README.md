dd94cc621c7b51e155e976cc6ce210cc04b74d7d
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-04-13 10:39:24 -0300

    feature: refactored create player profile form to put a create as guest option

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-## Use Cases
+## TODO
 
-- [x] Home page deve apenas listas todas as partidas
-- [x] Pagina que deve mostrar uma partida detalhadamente com todos os jogadores presentes nela, gols, assitencias e substituiçoes de cada um
-- [x] Pagina que deve mostrar o perfil de cada jogador cadastrado detalhadamente
-- [ ] Pagina que lista todos os jogadores de forma breve
-- [ ] Um dashbord que vai ter opçoes de CRUD em todos os jogos, jogadores e perfis
-
-
-## To-do
-- [x] Funcionalidade de criar uma partida e enviar o usuario para a rota dela
-- [ ] Funcionalidade de selecionar os jogadores e iniciar a partida
-- [ ] Funcionalidade de editar os stats da partida
-
-# Dashboard
-- [ ] manage a game in progress
-- [ ] CRUD all games
-- [ ] CRUD all players profile
+- [ ] Funcionalidade para escolher a função do jogadore dentro da partida (ex: goleiro jogando na linha)
+- [ ] Funcionalidade para somar gol contra
 - [ ] 
--- src/pages/dashboard/jogadores/create.tsx
+++ src/pages/dashboard/jogadores/create.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
@@ -7,16 +8,27 @@ import { PlayerProfile } from '../../../..';
 import { api } from '../../../services/axios';
 type Inputs = {
   name: string;
-  whiteShirt: FileList;
-  greenShirt: FileList;
+  photo: FileList;
+  guest: boolean;
+  color: string;
 };
 export default function CreatePlayer() {
   const { push } = useRouter();
-  const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = async ({ name }) => {
+  const { register, handleSubmit, formState, watch } = useForm<Inputs>();
+
+  const { photo, name } = watch();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ guest, name }) => {
+    const data = {
+      name,
+      role: guest ? 'GUEST' : 'PERMANENT',
+    };
+    console.log(data);
+    return;
     try {
       await api.post<PlayerProfile>('/players', {
         name,
+        role: guest ? 'GUEST' : 'PERMANENT',
       });
 
       push(`/dashboard/jogadores/`);
@@ -24,11 +36,13 @@ export default function CreatePlayer() {
       toast.error(err.message);
     }
   };
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col items-center">
-      <h1>Criar novo jogador</h1>
-      <div className="form-control w-[320px]  mx-auto gap-6">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
+      {photo && photo.length > 0 ? (
+        <img src={URL.createObjectURL(photo[0])} alt="" className="w-36 h-36 rounded-full my-5" />
+      ) : null}
+      <h1 className="mb-4">{name ? name : 'Criar novo jogador'}</h1>
+      <div className="form-control w-[320px]  mx-auto gap-2">
         <label className="input-group input-group-vertical ">
           <span>Nome</span>
           <input
@@ -38,22 +52,44 @@ export default function CreatePlayer() {
             {...register('name')}
           />
         </label>
-        {/* <label className="input-group input-group-vertical ">
-          <span>Foto com camisa branca</span>
+        <label className="flex items-center gap-4  justify-end">
+          <span className="label-text">Convidado</span>
+          <input {...register('guest')} type="checkbox" className="checkbox" />
+        </label>
+
+        {/* <label className=" flex gap-2 ">
+          <label htmlFor="photo" className="btn btn-primary w-fit">
+            Enviar foto
+          </label>
           <input
-            {...register('whiteShirt')}
+            {...register('photo')}
             type="file"
-            className="file-input file-input-bordered w-full max-w-xs"
+            id="photo"
+            className=" hidden file-input file-input-bordered file-input-primary w-full max-w-xs"
+            accept="image/*"
           />
+          <div className="flex items-center gap-2">
+            <label htmlFor="" className="flex gap-2">
+              Verde
+              <input
+                type="radio"
+                className="radio radio-primary"
+                value="verde"
+                {...(register('color'), { required: true })}
+              />
+            </label>
+            <label htmlFor="" className="flex gap-2">
+              Branco
+              <input
+                type="radio"
+                className="radio radio-white"
+                value="branco"
+                {...(register('color'), { required: true })}
+              />
+            </label>
+          </div>
         </label> */}
-        {/* <label className="input-group input-group-vertical ">
-          <span>Foto com camisa verde</span>
-          <input
-            {...register('greenShirt')}
-            type="file"
-            className="file-input file-input-bordered w-full max-w-xs"
-          />
-        </label> */}
+        <h1>{formState.errors.photo?.message}</h1>
         <button type="submit" className="btn btn-primary w-fit self-center mt-6">
           Criar
         </button>
