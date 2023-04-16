843f8c3f3b1e01a8006bb9704d8b245270af9b3d
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-04-16 10:30:47 -0300

    feature button to show/hide guest from the player list

--- src/pages/jogadores/index.tsx
+++ src/pages/jogadores/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -8,15 +10,30 @@ import { LoadingSpin } from '../../components/Loading';
 import { getProfileImage } from '../../functions/getProfileImage';
 const Jogadores = () => {
   const { data: players, isLoading } = useSWR<PlayerProfile[]>('/players');
+  const [isShowingGuest, setShowGuest] = useState<boolean>(false);
 
   const { push } = useRouter();
   if (isLoading) {
     return <LoadingSpin />;
   }
+  const permanentPlayers = players?.filter((player) => player.role === 'PERMANENT');
+  const guestPlayers = players?.filter((player) => player.role === 'GUEST');
   return (
     <div className="w-[95%] mx-auto">
+      <div className=" flex justify-end">
+        <div className="flex items-center gap-2">
+          <label htmlFor="show-guest">Mostrar convidados</label>
+          <input
+            type="checkbox"
+            id="show-guest"
+            defaultChecked={isShowingGuest}
+            className="checkbox checkbox-primary"
+            onChange={() => setShowGuest((prev) => !prev)}
+          />
+        </div>
+      </div>
       <div className="flex flex-col ">
-        {players?.map((player) => (
+        {permanentPlayers?.map((player) => (
           <div
             key={player.id}
             onClick={() => push(`/jogadores/${player.slug}`)}
@@ -48,6 +65,41 @@ const Jogadores = () => {
             </Link>
           </div>
         ))}
+        {isShowingGuest && <h3 className="self-center mt-4">Convidados</h3>}
+        {isShowingGuest &&
+          guestPlayers?.map((player) => (
+            <div key={player.id}>
+              <div
+                onClick={() => push(`/jogadores/${player.slug}`)}
+                className="flex items-center hover:bg-neutral  transition-all justify-between gap-5 py-2 border-b-[1px] border-neutral-900 w-[90%] mx-auto cursor-pointer"
+              >
+                <div className="flex items-center gap-4">
+                  <Link
+                    role={'Foto de perfil do jogador'}
+                    href={`/jogadores/${player.slug}`}
+                    className="cursor-pointer"
+                  >
+                    <Image
+                      src={getProfileImage(player)}
+                      alt="Foto de perfil do jogador"
+                      width={60}
+                      height={60}
+                      className="rounded-full w-12 h-12 "
+                    ></Image>
+                  </Link>
+                  <h1 className="min-w-20">
+                    {player.shirtNumber || 0} - {player.name}
+                  </h1>
+                </div>
+                <Link
+                  href={`/jogadores/${player.slug}`}
+                  className="link text-[12px] self-end justify-end"
+                >
+                  Ver perfil completo
+                </Link>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
