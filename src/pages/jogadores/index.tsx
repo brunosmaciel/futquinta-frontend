2ba0f19f9eacaf3636b18665683344711804e856
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-05-26 13:44:34 -0300

    fix/ organize player page by number in desc order

--- src/pages/jogadores/index.tsx
+++ src/pages/jogadores/index.tsx
@@ -34,38 +34,41 @@ const Jogadores = () => {
         </div>
       </div>
       <div className="flex flex-col ">
-        {permanentPlayers?.map((player) => (
-          <div
-            key={player.id}
-            onClick={() => push(`/jogadores/${player.slug}`)}
-            className="flex items-center hover:bg-neutral  transition-all justify-between gap-5 py-2 border-b-[1px] border-neutral-900 w-[90%] mx-auto cursor-pointer"
-          >
-            <div className="flex items-center gap-4">
-              <Link
-                role={'Foto de perfil do jogador'}
-                href={`/jogadores/${player.slug}`}
-                className="cursor-pointer"
-              >
-                <Image
-                  src={getProfileImage(player)}
-                  alt="Foto de perfil do jogador"
-                  width={60}
-                  height={60}
-                  className="rounded-full w-12 h-12 "
-                ></Image>
-              </Link>
-              <h1 className="min-w-20">
-                {player.shirtNumber || 0} - {player.name}
-              </h1>
-            </div>
-            <Link
-              href={`/jogadores/${player.slug}`}
-              className="link text-[12px] self-end justify-end"
+        {permanentPlayers
+          ?.sort((a, b) => (a.shirtNumber < b.shirtNumber ? -1 : 1))
+          .filter((player) => player.name !== 'Convidados')
+          .map((player) => (
+            <div
+              key={player.id}
+              onClick={() => push(`/jogadores/${player.slug}`)}
+              className="flex items-center hover:bg-neutral  transition-all justify-between gap-5 py-2 border-b-[1px] border-neutral-900 w-[90%] mx-auto cursor-pointer"
             >
-              Ver perfil completo
-            </Link>
-          </div>
-        ))}
+              <div className="flex items-center gap-4">
+                <Link
+                  role={'Foto de perfil do jogador'}
+                  href={`/jogadores/${player.slug}`}
+                  className="cursor-pointer"
+                >
+                  <Image
+                    src={getProfileImage(player)}
+                    alt="Foto de perfil do jogador"
+                    width={60}
+                    height={60}
+                    className="rounded-full w-12 h-12 "
+                  ></Image>
+                </Link>
+                <h1 className="min-w-20">
+                  {player.shirtNumber || 0} - {player.name}
+                </h1>
+              </div>
+              <Link
+                href={`/jogadores/${player.slug}`}
+                className="link text-[12px] self-end justify-end"
+              >
+                Ver perfil completo
+              </Link>
+            </div>
+          ))}
         {isShowingGuest && <h3 className="self-center mt-4">Convidados</h3>}
         {isShowingGuest &&
           guestPlayers?.map((player) => (
