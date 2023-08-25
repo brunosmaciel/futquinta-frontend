0e6d8b0d33c114ca79e5b4178bf610c3f540c866
Bruno Maciel <brunosazanamaciel@gmail.com>
2023-08-25 17:07:00 -0300

    new mobile menu

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -1,31 +1,52 @@
-import { ReactNode, useContext } from 'react';
+import { ReactNode, useContext, useEffect, useRef } from 'react';
 
 import Link from 'next/link';
 
 import { AuthContext } from '../../contexts/AuthContex';
+import { useRouter } from 'next/router';
+import { MedalIcon, ShirtIcon } from 'lucide-react';
+import BallIcon from '../ui/BallIcon';
 
 export const NavBar = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+  const menu = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (menu.current) {
+      menu.current.checked = false;
+    }
+  }, [router.pathname]);
+
   const { isLoggedIn } = useContext(AuthContext);
   return (
     <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <input ref={menu} id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
         <div className="w-full navbar ">
           <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+            <label htmlFor="my-drawer-3" className="btn btn-circle btn-ghost swap swap-rotate">
+              {/* this hidden checkbox controls the state */}
+
+              {/* hamburger icon */}
               <svg
+                className="swap-off fill-current"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
+                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+              </svg>
+
+              {/* close icon */}
+              <svg
+                className="swap-on fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512"
+              >
+                <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
               </svg>
             </label>
           </div>
@@ -70,12 +91,38 @@ export const NavBar = ({ children }: { children: ReactNode }) => {
       <div className="drawer-side">
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200">
-          {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
+          <div className=" flex items-center gap-2">
+            <Link className="btn btn-ghost normal-case text-xl " href={'/'}>
+              FutQuinta <span className="logo">Score</span>
+            </Link>
+            {isLoggedIn ? (
+              <div className="navbar-end mr-2">
+                <Link href={'/dashboard'} className="btn btn-outline btn-sm">
+                  Admin
+                </Link>
+              </div>
+            ) : (
+              <div className="navbar-end">
+                <Link className="btn btn-primary btn-sm" href="/login">
+                  Login
+                </Link>
+              </div>
+            )}
+          </div>
+          <li className="text-lg mt-2">
+            <Link href={`/jogadores`}>
+              <ShirtIcon size={20} /> Jogadores{' '}
+            </Link>
           </li>
-          <li>
-            <a>Sidebar Item 2</a>
+          <li className="text-lg mt-2">
+            <Link href={`/jogos`}>
+              <BallIcon className="h-5 w-5" /> Partidas
+            </Link>
+          </li>
+          <li className="text-lg mt-2">
+            <Link href={`/rankings`}>
+              <MedalIcon size={20} /> Rankings
+            </Link>
           </li>
         </ul>
       </div>
