a53bda72843d16ef240f27ba07e9c6b9a3708582
Bruno Sazana <brunosazanamaciel@gmail.com>
2024-11-13 21:07:57 -0300

    final commit

--- src/components/PlayerProfileImage.tsx
+++ src/components/PlayerProfileImage.tsx
@@ -1,9 +1,12 @@
 import { SetStateAction } from 'react';
+import { champions } from '../../public/campeoes';
 
 import Image from 'next/image';
 
 import { PlayerProfile } from '../..';
 import { profilePicturePlaceholder } from '../utils/profilePicturePlaceholder';
+import { cn } from '../utils/cn';
+import { Crown } from 'lucide-react';
 interface IPlayerProfileImageProps {
   player: PlayerProfile;
   setIsOpen: (value: SetStateAction<boolean>) => void;
@@ -22,19 +25,32 @@ export function PlayerProfileImage({ player, setIsOpen }: IPlayerProfileImagePro
   }
 
   return (
-    <div className="border-[#ffbf00] flex items-center justify-center rounded-full w-38 h-38 border-4 bg-red-500">
-      <Image
-        src={
-          player.currentPicture === 'WHITE'
-            ? player.whiteShirtpicture || ''
-            : player.greenShirtpicture || ''
-        }
-        alt="Foto de perfil do jogador"
-        width={300}
-        height={300}
-        className="rounded-full w-36 h-36 border-2"
-        onClick={() => setIsOpen(true)}
-      />
-    </div>
+    <>
+      <div
+        className={` flex flex-col items-center justify-center rounded-full w-38 h-38 ${
+          champions.includes(player.id) ? cn('border-[#ffbf00] border-4') : null
+        }`}
+      >
+        <Image
+          src={
+            player.currentPicture === 'WHITE'
+              ? player.whiteShirtpicture || ''
+              : player.greenShirtpicture || ''
+          }
+          alt="Foto de perfil do jogador"
+          width={300}
+          height={300}
+          className="rounded-full w-36 h-36 border-2"
+          onClick={() => setIsOpen(true)}
+        />
+      </div>
+      {champions.includes(player.id) ? (
+        <>
+          <h1 className="badge bg-[#ffbf00] text-black p-[12px] text-sm font-bold">
+            <Crown className="mr-2" /> Campeão Torneio Interno 2024
+          </h1>
+        </>
+      ) : null}
+    </>
   );
 }
